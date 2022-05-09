565c9c0bd4b1cac5e79e7560ee79a222f8eb45ea
ugur sag <ug.sagsoz@gmail.com>
2022-05-09 11:57:39 +0100

    done

--- src/Components/Main-Section.js
+++ src/Components/Main-Section.js
@@ -3,12 +3,17 @@ import "./Style/Main-Section.css"
 
 export default function MainSection() {
   return (
-    <div>
+    <div className="">
       <div class="item_description">
-        <div class="image">
+      <div class="desc">
+          <h5>BRAND,DEV,ECOM,MARKETING</h5>
+        <h1>We unlesh</h1>
+        <h1>Business Potential</h1>
+        <p>We create brand experiences which are memorable and distinct.Our experienced
+          team create and develop brands with personality and resonance
+        </p>
         </div>
-        <div class="desc">
-          Description here
+        <div class="image">
         </div>
       </div>
     </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,11 +8,11 @@ function Navbar() {
             <div><img className="logo-image"src={logo} alt="Logo" /></div>
             <div className="list">
                 <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#news">News</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#about">About</a></li>
+                    <li><a className="list-a" href="#home">Services</a></li>
+                    <li><a className="list-a" href="#news">Work</a></li>
+                    <li><a className="list-a" href="#contact">About</a></li>
+                    <li><a className="list-a" href="#about">Blog</a></li>
+                    <li><a className="list-a" href="#about">Contact</a></li>
                 </ul>
             </div>
         </div>
